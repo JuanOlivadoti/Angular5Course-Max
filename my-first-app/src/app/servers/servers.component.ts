f1fc479545c35821ba1fe306911f177731d5cd40
DESKTOP-54DOS0H\OLIVA <jp.olivadoti@gmail.com>
2017-12-12 12:40:06 +0100

    selectors

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
+  // selector: '[app-servers]', //seleccionar por atributos <div app-servers>
+  // selector: '.app-servers', //seleccionar por clase <div class="app-servers"> //no funciona con id
   // template: `<app-server></app-server>
   //             <app-server></app-server>`, //cuando el html es pequeño.
   templateUrl: './servers.component.html',
