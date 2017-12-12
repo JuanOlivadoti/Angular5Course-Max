c29abfc1a3f887a846219bd250dd904413d043e5
DESKTOP-54DOS0H\OLIVA <jp.olivadoti@gmail.com>
2017-12-12 12:06:56 +0100

    Server Compontent Creation

---
+++ my-first-app/src/app/server/server.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-sever',
+    template: './server.component.html'
+})
+
+export class ServerComponent {
+    constructor(parameters) {
+        
+    }
+}
