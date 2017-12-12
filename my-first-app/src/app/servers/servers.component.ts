b3ef15700bf2a591052f6df768f9a6dc87d2d137
DESKTOP-54DOS0H\OLIVA <jp.olivadoti@gmail.com>
2017-12-12 12:35:57 +0100

    templateUrl template styleUrls styles

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
+  // template: `<app-server></app-server>
+  //             <app-server></app-server>`, //cuando el html es pequeño.
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
