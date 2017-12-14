116c08b11d1ee481857ea2d5dec3ef4c040a0165
DESKTOP-54DOS0H\OLIVA <jp.olivadoti@gmail.com>
2017-12-14 18:14:22 +0100

    basic-assignment-3-Solution

--- basics-assignment-3-start/src/app/app.component.ts
+++ basics-assignment-3-start/src/app/app.component.ts
@@ -6,4 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  pToggler = false;
+  // counter = 0;
+  clickArray = [];
+
+  onBtnClick() {
+    // (this.counter % 2) ? this.pToggler = true : this.pToggler = false;
+    this.pToggler = !this.pToggler;
+    // this.counter++;
+    this.clickArray.push(this.clickArray.length + 1);
+  }
+
+  // getBgColor() {
+  //   console.log(this.clickArray.length);
+  //   return this.
+  // }
 }
