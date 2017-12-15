b489f8f8acd4da5e18620a454e0d325c266a89fe
DESKTOP-54DOS0H\OLIVA <jp.olivadoti@gmail.com>
2017-12-15 14:41:30 +0100

    view encapsulation

--- cmp-databinding-start/src/app/app.component.ts
+++ cmp-databinding-start/src/app/app.component.ts
@@ -6,8 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  serverElements = [{ type: 'server', name: 'TestServer', content: 'Just a test!' }];
+  serverElements = [];
 
+  onServerAdded(serverData: { serverName: string, serverContent: string }) {
+    this.serverElements.push({
+      type: 'server',
+      // name: this.newServerName,
+      name: serverData.serverName,
+      // content: this.newServerContent
+      content: serverData.serverContent
+    });
+  }
 
+  onBlueprintAdded(blueprintData: { serverName: string, serverContent: string }) {
+    this.serverElements.push({
+      type: 'blueprint',
+      // name: this.newServerName,
+      name: blueprintData.serverName,
+      // content: this.newServerContent
+      content: blueprintData.serverContent
+    });
+  }
 
 }
--- cmp-databinding-start/src/app/cockpit/cockpit.component.ts
+++ cmp-databinding-start/src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+// import { EventEmitter } from 'events';
 
 @Component({
   selector: 'app-cockpit',
@@ -6,7 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cockpit.component.css']
 })
 export class CockpitComponent implements OnInit {
-
+  @Output() serverCreated = new EventEmitter<{ serverName: string, serverContent: string }>();
+  @Output() blueprintCreated = new EventEmitter<{ serverName: string, serverContent: string }>();
   newServerName = '';
   newServerContent = '';
 
@@ -21,6 +23,10 @@ export class CockpitComponent implements OnInit {
     //   name: this.newServerName,
     //   content: this.newServerContent
     // });
+    this.serverCreated.emit({
+      serverName: this.newServerName,
+      serverContent: this.newServerContent
+    });
   }
 
   onAddBlueprint() {
@@ -29,6 +35,10 @@ export class CockpitComponent implements OnInit {
     //   name: this.newServerName,
     //   content: this.newServerContent
     // });
+    this.blueprintCreated.emit({
+      serverName: this.newServerName,
+      serverContent: this.newServerContent
+    })
   }
 
 }
