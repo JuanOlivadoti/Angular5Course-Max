32f168d7bab6427fb97a9603a0e03673b1ee81b1
DESKTOP-54DOS0H\OLIVA <jp.olivadoti@gmail.com>
2017-12-21 09:50:04 +0100

    Custom Structural directive

--- directives-start/src/app/better-highlight/better-highlight.directive.ts
+++ directives-start/src/app/better-highlight/better-highlight.directive.ts
@@ -1,13 +1,16 @@
-import { Directive, Renderer2, OnInit, ElementRef, HostListener, HostBinding } from '@angular/core';
-
+import { Directive, Renderer2, OnInit, ElementRef, HostListener, HostBinding, Input } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
+  @Input() defaultColor: string = 'cyan';
+  // @Input('appBetterHighlight') highlightColor: string = 'dodgerblue'; SOLO SI DESEO BINDEAR ELEMENTOS CON EL MISMO SELECTOR DE DIRECTIVAS
+  @Input() highlightColor: string = 'dodgerblue';
 
   // @HostBinding('style.backgroundColor') backgroundColor: string = 'transparent'; puedo declarar un valor default
-  @HostBinding('style.backgroundColor') backgroundColor: string;
+  // @HostBinding('style.backgroundColor') backgroundColor: string;
+  @HostBinding('style.backgroundColor') backgroundColor: string = this.defaultColor;
 
   constructor(private elRef: ElementRef, private renderer: Renderer2) {
 
@@ -26,7 +29,8 @@ export class BetterHighlightDirective implements OnInit {
     //   'background-color',
     //   'dodgerblue'
     // )
-    this.backgroundColor = 'dodgerblue';
+    // this.backgroundColor = 'dodgerblue';
+    this.backgroundColor = this.highlightColor;
   }
   @HostListener('mouseleave') mouseleave(eventData: Event) {
     // this.renderer.setStyle(
@@ -34,7 +38,8 @@ export class BetterHighlightDirective implements OnInit {
     //   'background-color',
     //   'lime'
     // )
-    this.backgroundColor = 'blue';
+    // this.backgroundColor = 'blue';
+    this.backgroundColor = this.defaultColor;
   }
 
 }
