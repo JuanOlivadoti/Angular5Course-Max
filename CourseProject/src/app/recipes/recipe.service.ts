d51b9f0102b8bcf884cfd86a2b516714ba2b508b
DESKTOP-54DOS0H\OLIVA <jp.olivadoti@gmail.com>
2018-01-15 12:12:30 +0100

    Adding ingredients to Recipes

--- CourseProject/src/app/recipes/recipe.service.ts
+++ CourseProject/src/app/recipes/recipe.service.ts
@@ -1,12 +1,25 @@
 import { Recipe } from "./recipe.model";
 import { EventEmitter } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
 
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe('A Test Recipe', 'This is a test', 'http://maxpixel.freegreatpicture.com/static/photo/1x/Food-Chicken-Menu-Recipe-Bacon-Dinner-Meat-Dish-2428926.jpg'),
-        new Recipe('Anotheer Test Recipe', 'Another test', 'https://static01.nyt.com/images/2015/08/14/dining/14ROASTEDSALMON/14ROASTEDSALMON-articleLarge.jpg')
+        new Recipe('Tasty Bacon Roll',
+            'Turkey roll with bacon',
+            'http://maxpixel.freegreatpicture.com/static/photo/1x/Food-Chicken-Menu-Recipe-Bacon-Dinner-Meat-Dish-2428926.jpg',
+            [
+                new Ingredient('Turkey', 2),
+                new Ingredient('bacon', 3)
+            ]),
+        new Recipe('Lemon Salmon',
+            'Pink salmon with lemon juice',
+            'https://static01.nyt.com/images/2015/08/14/dining/14ROASTEDSALMON/14ROASTEDSALMON-articleLarge.jpg',
+            [
+                new Ingredient('Salmon', 1),
+                new Ingredient('lemon', 5)
+            ])
     ];
 
     getRecipes() {
