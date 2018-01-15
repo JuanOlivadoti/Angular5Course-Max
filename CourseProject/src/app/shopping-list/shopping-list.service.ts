3cfd71013484399e52430e248f221d2400f85396
DESKTOP-54DOS0H\OLIVA <jp.olivadoti@gmail.com>
2018-01-15 10:54:52 +0100

    Shopping list service Update list.

--- CourseProject/src/app/shopping-list/shopping-list.service.ts
+++ CourseProject/src/app/shopping-list/shopping-list.service.ts
@@ -1,3 +1,21 @@
-export class ShoppingListService {
+import { EventEmitter } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
 
+export class ShoppingListService {
+    // inform the component that new data is available
+    ingredientsChanged = new EventEmitter<Ingredient[]>();
+
+    private ingredients: Ingredient[] = [
+        new Ingredient('Apples', 5),
+        new Ingredient('Tomato', 10)
+    ];
+
+    getIngredients() {
+        return this.ingredients.slice();
+    };
+
+    addIngredient(ingredient: Ingredient) {
+        this.ingredients.push(ingredient);
+        this.ingredientsChanged.emit(this.ingredients.slice());
+    };
 }
