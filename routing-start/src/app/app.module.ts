b3a026e342fd69f094d8679fc7cd0636717e61d4
DESKTOP-54DOS0H\OLIVA <jp.olivadoti@gmail.com>
2018-01-23 10:53:36 +0100

    138 Controlling Navigation with Can Deactivate

--- routing-start/src/app/app.module.ts
+++ routing-start/src/app/app.module.ts
@@ -16,6 +16,7 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
 import { AppRoutingModule } from './app-routing-module';
 import { AuthGuard } from './auth-guard.service';
 import { AuthService } from './auth.service';
+import { CanDeactivateGuard } from './servers/edit-server/can-deactivate-guard.service';
 
 // const appRoutes: Routes = [
 //   { path: '', component: HomeComponent },
@@ -58,7 +59,7 @@ import { AuthService } from './auth.service';
     AppRoutingModule // trae todas las rutas del modulo de rutas.
     // RouterModule.forRoot(appRoutes)
   ],
-  providers: [ServersService, AuthService, AuthGuard],
+  providers: [ServersService, AuthService, AuthGuard, CanDeactivateGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
