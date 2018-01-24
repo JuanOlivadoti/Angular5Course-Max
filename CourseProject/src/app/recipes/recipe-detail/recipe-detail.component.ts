2b74507f7eaa4d80a5dd1ea7f9526bf77125a767
DESKTOP-54DOS0H\OLIVA <jp.olivadoti@gmail.com>
2018-01-24 17:12:20 +0100

    155 - Programatic navigation to the edit page

--- CourseProject/src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ CourseProject/src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,10 +1,9 @@
 // import { Component, OnInit, Input } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
-import { Params } from '@angular/router/src/shared';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -16,7 +15,7 @@ export class RecipeDetailComponent implements OnInit {
   recipe: Recipe;
   id: number;
 
-  constructor(private recipeService: RecipeService, private route: ActivatedRoute) { }
+  constructor(private recipeService: RecipeService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
     // const id = this.route.snapshot.params['id'];
@@ -31,5 +30,10 @@ export class RecipeDetailComponent implements OnInit {
 
   onAddToShoppinglist() {
     this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
+  }
+
+  onEditRecipe() {
+    this.router.navigate(['edit'], { relativeTo: this.route });
+    // this.router.navigate(['../', this.id, 'edit'], { relativeTo: this.route }); //Funciona igualmente que lo anterior
   }
 }
