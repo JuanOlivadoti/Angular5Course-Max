7257b558f8ff841b29ae3f82fc08889b9556126c
DESKTOP-54DOS0H\OLIVA <jp.olivadoti@gmail.com>
2018-02-07 10:45:11 +0100

    Using Subject to pass AND to listen to data. (Observable + Observer ... All-in-one)

--- observables-start/src/app/home/home.component.ts
+++ observables-start/src/app/home/home.component.ts
@@ -1,26 +1,56 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/Rx'
+import { Observer } from 'rxjs/Observer';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+  numberObservableSubscription: Subscription;
+  customObservableSubscription: Subscription;
 
   constructor() { }
 
   ngOnInit() {
     const myNumbers = Observable.interval(1000);
 
-    myNumbers.subscribe(
+    this.numberObservableSubscription = myNumbers.subscribe(
       (number: number) => {
         console.log(number);
 
       }
     );
+
+    const myObservable = Observable.create((observer: Observer<string>) => {
+      setTimeout(() => {
+        observer.next('first package');
+      }, 2000);
+      setTimeout(() => {
+        observer.next('second package');
+      }, 4000);
+      setTimeout(() => {
+        // observer.error('this does not work!');
+        observer.complete();
+      }, 5000);
+      setTimeout(() => {
+        observer.next('another package');
+      }, 6000);
+    });
+    this.customObservableSubscription = myObservable.subscribe(
+      (data: string) => { console.log(data); },
+      (error: string) => { console.log(error); },
+      () => { console.log('completed'); }
+    )
+  }
+
+  ngOnDestroy() {
+    this.numberObservableSubscription.unsubscribe();
+    this.customObservableSubscription.unsubscribe();
   }
 
 }
